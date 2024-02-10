3f14ee825797ff1bd351eacf4a281ea0e73c8f97
root <root@ThangFSD.localdomain>
2024-02-10 22:34:26 +0700

    darkmode-script fixed

--- script-darkmode.js
+++ script-darkmode.js
@@ -1,49 +1,50 @@
 let darkMode = localStorage.getItem("darkMode");
 
-const datetime = document.getElementById("date");
 const toggleTheme = document.querySelector(".toggle-theme");
 const toggleHourFt = document.querySelector('.toggle-12hrs');
-const hr = document.querySelector('.hr');
-const min = document.getElementsByClassName('min');
-const date = document.getElementById("date");
-const p = document.getElementsByTagName("p");
-const ampmE = document.getElementById("ampm");
+const theme1 = Array.from(document.getElementsByClassName("theme1"));
+const p = Array.from(document.getElementsByTagName("p"));
 
-
+// Enable darkmode 
 const enableDarkmode = () => {
     document.body.classList.add("darkmode");
-    date.classList.add("darkmode-text");
-    ampmE.style.color = "#eee";
-    hr.classList.add("darkmode-text");
-    min[0].classList.add("darkmode-text");
-    min[1].classList.add("darkmode-text");
-    p[0].classList.add("darkmode-text");
-    p[1].classList.add("darkmode-text");
-    toggleHourFt.style.border = "#eee solid";
-    toggleHourFt.style.setProperty("--before", "#eee solid");
-    
+    theme1.forEach(darkTheme);
+    p.forEach(darkTheme);
+    toggleHourDark(toggleHourFt);
 
     localStorage.setItem("darkMode", "enabled");
 };
 
+//Disable darkmode 
 const disableDarkmode = () => {
     document.body.classList.remove("darkmode");
-    date.classList.remove("darkmode-text");
-    hr.classList.remove("darkmode-text");
-    ampmE.style.color = "#302d2d";
-    min[0].classList.remove("darkmode-text");
-    min[1].classList.remove("darkmode-text");
-    p[0].classList.remove("darkmode-text");
-    p[1].classList.remove("darkmode-text");
-    toggleHourFt.style.border = "#302d2d solid";
-    toggleHourFt.style.setProperty("--before", "#302d2d solid");
-
+    theme1.forEach(lightTheme);
+    p.forEach(lightTheme);
+    toggleHourLight(toggleHourFt);
     localStorage.setItem("darkMode", null);
 };
 
-// toggleHourFt.addEventListener("click", () => {
-    // toggleHourFt.classList.toggle("active-h");
-// });
+// Toggle 12 hour theme
+function toggleHourLight(element) {
+    element.style.border = "#302d2d solid";
+    element.style.setProperty("--before", "#302d2d solid");
+} 
+
+// Toggle 12 hour theme  
+function toggleHourDark(element) {
+    element.style.border = "#eee solid";
+    element.style.setProperty("--before", "#000 solid");
+}
+
+//Darktheme 
+function darkTheme(element) {
+    element.style.color = "#eee";
+}
+
+//Lightheme
+function lightTheme(element) {
+    element.style.color = "#302d2d";
+}
 
 if (darkMode && darkMode === "enabled")
 {
